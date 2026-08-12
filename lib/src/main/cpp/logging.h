c0066cf906ceeb99fe93c5c1027ff1be3c6f6084
yanfyanqiu <61773313+yanfyanqiu@users.noreply.github.com>
2026-08-12 21:53:48 +0800

    fix(logging): android_log_is_loggable requires API 30; use simple branch below API 30

--- lib/src/main/cpp/logging.h
+++ lib/src/main/cpp/logging.h
@@ -23,7 +23,12 @@
 #endif
 
 static inline int ai_should_log(int prio) {
+#if __ANDROID_API__ >= 30
     return __android_log_is_loggable(prio, LOG_TAG, LOG_MIN_LEVEL);
+#else
+    (void)prio;
+    return 1;
+#endif
 }
 
 #if LOG_MIN_LEVEL <= ANDROID_LOG_VERBOSE
